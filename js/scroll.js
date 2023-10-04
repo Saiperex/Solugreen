fd9ad5a50efa4cd981e9a2dcae21c4e845394b76
Xavier <xaviramos668@gmail.com>
2023-10-04 18:54:56 -0300

    Eliminamos touch

--- js/scroll.js
+++ js/scroll.js
@@ -1,34 +1,28 @@
 const sectores = document.querySelectorAll('.sec');
-    let indiceActual = 0;
-    let valorTop = 100;
+let indiceActual = 0;
+let valorTop = 100;
 
-    var desplazarSectores = (deltaY) => 
-    {
-        if (deltaY > 0 && indiceActual < sectores.length) 
-        {
-            
-            if(valorTop==0)
-            {
-                indiceActual=indiceActual+1;
-                valorTop=100;
+const desplazarSectores = (deltaY) => {
+    if (window.innerWidth > 500) {
+        if (deltaY > 0 && indiceActual < sectores.length) {
+            if (valorTop === 0) {
+                indiceActual++;
+                valorTop = 100;
             }
-            valorTop=valorTop-10;
-        } else 
-        if (deltaY < 0 && indiceActual >= 0) 
-        {
-            
-            if(valorTop==100)
-            {
-                indiceActual=indiceActual-1;
-                valorTop=0;
+            valorTop -= 10;
+        } else if (deltaY < 0 && indiceActual >= 0) {
+            if (valorTop === 100) {
+                indiceActual--;
+                valorTop = 0;
             }
-            valorTop=valorTop+10;
+            valorTop += 10;
         }
-        sectores[indiceActual].style.top = valorTop+'vh';
-        console.log("moviendo el sec "+indiceActual+" Y el valor de top es"+valorTop)
-    };
+        sectores[indiceActual].style.top = valorTop + 'vh';
+        console.log("Moviendo el sec " + indiceActual + " Y el valor de top es " + valorTop);
+    }
+};
 
-    document.addEventListener('wheel', (event) => {
-        const deltaY = event.deltaY;
-        desplazarSectores(deltaY);
-    });
+document.addEventListener('wheel', (event) => {
+    const deltaY = event.deltaY;
+    desplazarSectores(deltaY);
+});
