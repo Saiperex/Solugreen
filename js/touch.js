b2ee155f539dd84e5a808195f461e2e60766ad5e
Xavier <xaviramos668@gmail.com>
2023-10-04 18:19:02 -0300

    touch v2

--- js/touch.js
+++ js/touch.js
@@ -1,41 +1,41 @@
 let touchStartY = null;
 
-    var desplazarSectores = (deltaY) => 
-    {
-        if (deltaY > 0 && indiceActual < sectores.length) 
-        {
-            if(valorTop === 0)
-            {
-                indiceActual++;
-                valorTop = 100;
-            }
-            valorTop -= 10;
-        } else 
-        if (deltaY < 0 && indiceActual >= 0) 
-        {
-            if(valorTop === 100)
-            {
-                indiceActual--;
-                valorTop = 0;
-            }
-            valorTop += 10;
+var desplazarSectores = (deltaY) => {
+    if (deltaY > 0 && indiceActual < sectores.length) {
+        if (valorTop === 0) {
+            indiceActual++;
+            valorTop = 100;
         }
-        sectores[indiceActual].style.top = valorTop + 'vh';
-        console.log("moviendo el sec " + indiceActual + " Y el valor de top es " + valorTop)
-    };
-    document.addEventListener('touchstart', (event) => {
-        touchStartY = event.touches[0].clientY;
-    });
-
-    document.addEventListener('touchmove', (event) => {
-        if (touchStartY !== null) {
-            const touchEndY = event.touches[0].clientY;
-            const deltaY = touchEndY - touchStartY;
-            touchStartY = touchEndY;
-            desplazarSectores(deltaY);
+        valorTop -= 10;
+    } else if (deltaY < 0 && indiceActual >= 0) {
+        if (valorTop === 100) {
+            indiceActual--;
+            valorTop = 0;
         }
-    });
+        valorTop += 10;
+    }
+    sectores[indiceActual].style.top = valorTop + 'vh';
+    console.log("Moviendo el sec " + indiceActual + " Y el valor de top es " + valorTop);
+};
 
-    document.addEventListener('touchend', () => {
-        touchStartY = null;
-    });
+document.addEventListener('wheel', (event) => {
+    const deltaY = event.deltaY;
+    desplazarSectores(deltaY);
+});
+
+document.addEventListener('touchstart', (event) => {
+    touchStartY = event.touches[0].clientY;
+});
+
+document.addEventListener('touchmove', (event) => {
+    if (touchStartY !== null) {
+        const touchEndY = event.touches[0].clientY;
+        const deltaY = touchStartY - touchEndY; // Invierte el signo aquí
+        touchStartY = touchEndY;
+        desplazarSectores(deltaY);
+    }
+});
+
+document.addEventListener('touchend', () => {
+    touchStartY = null;
+});
